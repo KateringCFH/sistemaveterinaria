ab1d89b529cc3b8813fc4d69bb118d58b4247f56
KateringCFH <kcristalfh@gmail.com>
2018-05-29 08:17:54 -0400

    dash 1/2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,105 +1,138 @@
 <?php
-$c= Auth::user()->
-cargo;
+$c= Auth::user()->cargo;
 ?>
 @extends('layouts.'.$c)
 @section('contenido')
-<section id="main-content">
-    <section class="wrapper">
-        <h3>
-            <i class="fa fa-angle-right">
-            </i>
-            Chartjs Charts
-        </h3>
-        <!-- page start-->
-        <div class="tab-pane" id="chartjs">
-            <div class="row mt">
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Doughnut
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="doughnut" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Line
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="line" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt">
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Radar
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="radar" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Polar Area
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="polarArea" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt">
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Bar
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="bar" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="content-panel">
-                        <h4>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            Pie
-                        </h4>
-                        <div class="panel-body text-center">
-                            <canvas height="300" id="pie" width="400">
-                            </canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- page end-->
-    </section>
-</section>
-<!-- /MAIN CONTENT -->
+  <section id="main-content">
+          <section class="wrapper">
+					<div class="row mt">
+                      <!--CUSTOM CHART START -->
+                      <div class="border-head">
+                          <h3>VISITS</h3>
+                      </div>
+                      <div class="custom-bar-chart">
+                          <ul class="y-axis">
+                              <li><span>10.000</span></li>
+                              <li><span>8.000</span></li>
+                              <li><span>6.000</span></li>
+                              <li><span>4.000</span></li>
+                              <li><span>2.000</span></li>
+                              <li><span>0</span></li>
+                          </ul>
+                          <div class="bar">
+                              <div class="title">JAN</div>
+                              <div class="value tooltips" data-original-title="8.500" data-toggle="tooltip" data-placement="top">85%</div>
+                          </div>
+                          <div class="bar ">
+                              <div class="title">FEB</div>
+                              <div class="value tooltips" data-original-title="5.000" data-toggle="tooltip" data-placement="top">50%</div>
+                          </div>
+                          <div class="bar ">
+                              <div class="title">MAR</div>
+                              <div class="value tooltips" data-original-title="6.000" data-toggle="tooltip" data-placement="top">60%</div>
+                          </div>
+                          <div class="bar ">
+                              <div class="title">APR</div>
+                              <div class="value tooltips" data-original-title="4.500" data-toggle="tooltip" data-placement="top">45%</div>
+                          </div>
+                          <div class="bar">
+                              <div class="title">MAY</div>
+                              <div class="value tooltips" data-original-title="3.200" data-toggle="tooltip" data-placement="top">32%</div>
+                          </div>
+                          <div class="bar ">
+                              <div class="title">JUN</div>
+                              <div class="value tooltips" data-original-title="6.200" data-toggle="tooltip" data-placement="top">62%</div>
+                          </div>
+                          <div class="bar">
+                              <div class="title">JUL</div>
+                              <div class="value tooltips" data-original-title="7.500" data-toggle="tooltip" data-placement="top">75%</div>
+                          </div>
+                      </div>
+                      <!--custom chart end-->
+					</div><!-- /row -->
+
+{{-- 
+
+                  <div class="col-lg-3 ds">
+                    <!--COMPLETED ACTIONS DONUTS CHART-->
+
+                        <!-- CALENDAR-->
+                        <div id="calendar" class="mb">
+                            <div class="panel green-panel no-margin">
+                                <div class="panel-body">
+                                    <div id="date-popover" class="popover top" style="cursor: pointer; disadding: block; margin-left: 33%; margin-top: -50px; width: 175px;">
+                                        <div class="arrow"></div>
+                                        <h3 class="popover-title" style="disadding: none;"></h3>
+                                        <div id="date-popover-content" class="popover-content"></div>
+                                    </div>
+                                    <div id="my-calendar"></div>
+                                </div>
+                            </div>
+                        </div><!-- / calendar -->
+
+                  </div><!-- /col-lg-3 --> --}}
+
+
+                  <div class="tab-pane" id="chartjs">
+                  <div class="row mt">
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Doughnut</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="doughnut" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Line</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="line" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row mt">
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Radar</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="radar" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Polar Area</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="polarArea" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row mt">
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Bar</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="bar" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-lg-6">
+                          <div class="content-panel">
+							  <h4><i class="fa fa-angle-right"></i> Pie</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="pie" height="300" width="400"></canvas>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+
+          </section>
+      </section>
+
+
 @endsection
